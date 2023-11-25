714d6e4b1e912d42a8018bdd27fb4825215ed932
milad-vht <m.va1371@gmail.com>
2023-11-25 18:56:06 +0330

    Add files via upload

---
+++ class/Session14/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+/*----------------------------functions
+برای عدم رعایت تکرار کد بکار میروند
+
+*/
+
+
+function baghimande($x,$y){
+return $x % $y;
+
+}
+
+$result = baghimande(5,2);
+
+echo $result;
+?>
