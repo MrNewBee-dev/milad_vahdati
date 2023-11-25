5d17f59ffacb82efc127203422c1cc691b70bdd0
milad-vht <m.va1371@gmail.com>
2023-11-25 19:58:19 +0330

    Add files via upload

---
+++ Project/phpProject/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>معرفی سایت شخصی</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="background-body">
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">صفحه اصلی</a></li>
+                <li><a href="#summary">بیوگرافی</a></li>
+                <li><a href="#skills">مهارتها</a></li>
+                <li><a href="#contact">پیام به من </a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="card" id="summary">
+            <div class="headingcard"><strong>بیوگرافی</strong> </div>
+            <div class="container">
---
+++ Project/phpProject/project.php
@@ -0,0 +1,59 @@
+<?php include('header.php')?>
+                <h4><b>میلاد وحدتی</b></h4>
+                <p>در حال حاضر کارمند صرافی</p>
+                <p> فارغ التحصیل رشته کامپیوتر دانشگاه
+                    آزاد اسلامی مشهد
+                </p>
+            </div>
+        </section>
+        <section class="card" id="skills">
+            <div class="headingcard"><strong>مهارت ها</strong> </div>
+            <div class="container">
+                <table>
+                    <tr>
+                        <th>شماره</th>
+                        <th>مهارت</th>
+                        <th>نمره</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>microsoft word</td>
+                        <td>متوسط</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>microsoft power-point</td>
+                        <td>متوسط</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>html</td>
+                        <td>ضعیف</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>css</td>
+                        <td>ضعیف</td>
+                    </tr>
+                </table>
+            </div>
+
+        </section>
+        <section class="card" id="contact">
+            <div class="headingcard"><strong>پیام به من</strong> </div>
+            <div class="container">
+                <form id="formmessage">
+                    <label for="fname">نام کاربری</label>
+                    <input type="text" id="fname" name="fname">
+                    <label for="lname">ایمیل</label>
+                    <input type="text" id="lname" name="lname">
+                    <label for="lname">پیام</label>
+                    <textarea name="message" id="" cols="30" rows="10" placeholder="پیام متنی را وارد کنید"></textarea>
+                  </form>
+            </div>
+        </section>
+    </main>
+<?php include('footer.php')?>
+</body>
+
+</html>
